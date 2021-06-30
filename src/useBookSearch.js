ccd7f5a1873eb6b0635f2176a90643b96476cde4
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-30 13:53:37 +0300

    Use axios cancelToken to stop searching every time a user types

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -4,9 +4,18 @@ import axios from 'axios'
 const useBookSearch = (query, pageNumber) => {
     const url = `http://openlibrary.org/search.json?q=${query}&page=${pageNumber}`
     useEffect(() => {
-        axios.get(url).then( res => {
+        let cancel;
+        const  params = { q: query, page: pageNumber}
+        axios({
+            method: 'GET',
+            url: 'http://openlibrary.org/search.json',
+           params,
+            cancelToken: new axios.CancelToken( c => cancel = c)
+        }).then( res => {
             console.log(res.data)
         })
+
+        return () => cancel()
     }, [query, pageNumber])
     return null
 }
